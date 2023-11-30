f64c8b9a75ed4db2b8eb9ac9f03f5f515838086e
Zadjil Sachet <zadjil.sachet@autorek.com>
2023-11-30 18:36:01 +0000

    Example code for SonarSource community ticket
    Example code for SonarSource community ticket https://community.sonarsource.com/t/ad0001-errors-when-using-warningsnotaserrors/105213

---
+++ SonarLint48/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarLint48
+{
+	internal static class Program
+	{
+		static void Main()
+		{
+			string nullItem = null;
+			bool value = false;
+
+			Console.WriteLine(nullItem.ToUpper());
+
+			if (nullItem == null) Console.WriteLine(value);
+			if (nullItem != null) Console.WriteLine(value);
+
+			switch (nullItem)
+			{
+				case "a":
+					Console.WriteLine("yes");
+					value = true;
+					break;
+				case "b":
+					Console.WriteLine("no");
+					value = false;
+					break;
+				default:
+					Console.WriteLine("yes");
+					value = true;
+					break;
+			}
+
+			Console.WriteLine(value);
+
+		}
+	}
+}
---
+++ SonarLintNet8/Program.cs
@@ -0,0 +1,25 @@
+﻿string? nullItem = null;
+bool value = false;
+
+Console.WriteLine(nullItem.ToUpper());
+
+if (nullItem == null) Console.WriteLine(value);
+if (nullItem != null) Console.WriteLine(value);
+
+switch (nullItem)
+{
+	case "a":
+		Console.WriteLine("yes");
+		value = true;
+		break;
+	case "b":
+		Console.WriteLine("no");
+		value = false;
+		break;
+	default:
+		Console.WriteLine("yes");
+		value = true;
+		break;
+}
+
+Console.WriteLine(value);
